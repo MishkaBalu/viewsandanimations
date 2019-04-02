8b52620736e4ca9be47e6fb9c1ad62c7bc163451
Leonid Shkolnyk <duolts1997@gmail.com>
2019-04-02 13:10:02 +0300

    loading animation

--- ButtonShield.playground/Contents.swift
+++ ButtonShield.playground/Contents.swift
@@ -1,12 +1,6 @@
-//: # ButtonShield
-//: A demo playground that demonstrates how to use Core Animation layers
-//: to create a fun button, shamelessly stolen from ExpressVPN
-//: > Icon made by [Icon Works](https://www.flaticon.com/authors/icon-works) from [www.flaticon.com](https://www.flaticon.com/) is licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/)
 
 import UIKit
 import PlaygroundSupport
-
-//: ### Extensions to store constants
 
 fileprivate extension CGFloat {
   static var outerCircleRatio: CGFloat = 0.8
@@ -21,6 +15,25 @@ fileprivate extension Double {
 
 
 class ButtonView: UIView {
+    enum State {
+        case off
+        case inProgress
+        case on
+    }
+    
+    public var state: State = .off {
+        didSet {
+            switch state {
+            case .inProgress:
+                showInProgress(true)
+            case .on:
+                showInProgress(false)
+            case .off:
+                showInProgress(false)
+            }
+        }
+    }
+    
     private let buttonLayer = CALayer()
     private lazy var innerCircle: CAShapeLayer = {
         let layer = CAShapeLayer()
@@ -32,7 +45,7 @@ class ButtonView: UIView {
         
         layer.shadowRadius = 15
         layer.shadowOpacity = 0.1
-        layer.shadowOffset = CGSize(width: 15, height: 10)
+        layer.shadowOffset = CGSize(width: 15, height: 0)
         layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         return layer
     }()
@@ -53,6 +66,21 @@ class ButtonView: UIView {
         return layer
     }()
     
+    private lazy var inProgressLayer: CAGradientLayer = {
+        let layer = CAGradientLayer()
+        layer.colors = [#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1), UIColor(white: 1, alpha: 0)].map { $0.cgColor }
+        layer.frame = CGRect(centre: buttonLayer.bounds.centre, size: buttonLayer.bounds.size.rescale(CGFloat.inProgressRatio))
+        layer.locations = [0 , 0.7].map { NSNumber(floatLiteral: $0) }
+        
+        let mask = CAShapeLayer()
+        mask.path = UIBezierPath(ovalIn: layer.bounds).cgPath
+        layer.mask = mask
+        
+        layer.isHidden = true
+        
+        return layer
+    }()
+    
     private func createBadgeMaskLayer() -> CAShapeLayer {
         let mask = CAShapeLayer()
         mask.path = UIBezierPath.badgePath.cgPath
@@ -60,8 +88,6 @@ class ButtonView: UIView {
         mask.transform = CATransform3DMakeScale(scale, scale, 1)
         return mask
     }
-    
-    
     
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -76,17 +102,49 @@ class ButtonView: UIView {
   private func configureLayers() {
     backgroundColor = #colorLiteral(red: 0.9600390625, green: 0.9600390625, blue: 0.9600390625, alpha: 1)
     buttonLayer.frame = bounds.largestContainedSquare.offsetBy(dx: 0, dy: -20)
-    buttonLayer.addSublayer(badgeLayer)
+//    buttonLayer.addSublayer(badgeLayer)
     buttonLayer.addSublayer(outerCircle)
+    buttonLayer.addSublayer(inProgressLayer)
     buttonLayer.addSublayer(innerCircle)
+    
+    layer.addSublayer(badgeLayer)
     layer.addSublayer(buttonLayer)
   }
+    private func showInProgress(_ show: Bool = true) {
+        if show {
+            inProgressLayer.isHidden = false
+            let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+            animation.fromValue = 0
+            animation.toValue = 2 * Double.pi
+            animation.duration = Double.inProgressPeriod
+            animation.repeatCount = .greatestFiniteMagnitude
+            inProgressLayer.add(animation, forKey: "inProgressAnimation")
+        } else {
+            inProgressLayer.isHidden = true
+            inProgressLayer.removeAnimation(forKey: "inProgressAnimation")
+        }
+    }
 }
-
-//: ### Present the button
 
 let aspectRatio = UIBezierPath.badgePath.bounds.width / UIBezierPath.badgePath.bounds.height
 let button = ButtonView(frame: CGRect(x: 0, y: 0, width: 300, height: 300 / aspectRatio))
 
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = button
+
+let connection = PseudoConnection(connectionTime: 5) { (state) in
+    switch state {
+    case .disconnected:
+        print("Disconnected")
+        button.state = .off
+    case .connecting:
+        print("Connecting")
+        button.state = .inProgress
+    case .connected:
+        print("Connected")
+        button.state = .on
+    }
+}
+
+let gesture = UITapGestureRecognizer(target: connection, action: #selector(PseudoConnection.toggle))
+button.addGestureRecognizer(gesture)
