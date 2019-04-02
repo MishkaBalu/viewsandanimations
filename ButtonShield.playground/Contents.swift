e0ebc045dcbbd50113ee843bce9a1f58ae207d36
Leonid Shkolnyk <duolts1997@gmail.com>
2019-04-02 12:16:56 +0300

    gradient background

--- ButtonShield.playground/Contents.swift
+++ ButtonShield.playground/Contents.swift
@@ -40,8 +40,16 @@ class ButtonView: UIView {
     private lazy var outerCircle: CAShapeLayer = {
         let layer = CAShapeLayer()
         layer.path = Utils.pathForCircleInRect(rect: buttonLayer.bounds, scaled: CGFloat.outerCircleRatio)
-        layer.fillColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        layer.fillColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
         layer.opacity = 0.4
+        return layer
+    }()
+    
+    private lazy var badgeLayer: CAGradientLayer = {
+        let layer = CAGradientLayer()
+        layer.colors = [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)].map { $0.cgColor }
+        layer.frame = self.layer.bounds
+        
         return layer
     }()
     
@@ -58,6 +66,7 @@ class ButtonView: UIView {
   private func configureLayers() {
     backgroundColor = #colorLiteral(red: 0.9600390625, green: 0.9600390625, blue: 0.9600390625, alpha: 1)
     buttonLayer.frame = bounds.largestContainedSquare.offsetBy(dx: 0, dy: -20)
+    buttonLayer.addSublayer(badgeLayer)
     buttonLayer.addSublayer(outerCircle)
     buttonLayer.addSublayer(innerCircle)
     layer.addSublayer(buttonLayer)
@@ -71,5 +80,3 @@ let button = ButtonView(frame: CGRect(x: 0, y: 0, width: 300, height: 300 / aspe
 
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = button
-
-
